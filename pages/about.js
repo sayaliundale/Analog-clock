471970c3456c7257dc60f1e3d8f372e99ea48874
NanditaNikam <nandita.nikam_comp2020@pccoer.in>
2023-02-12 18:25:39 +0530

    updated aboutPage

--- pages/about.js
+++ pages/about.js
@@ -7,9 +7,7 @@ const About = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>About ACMxPCCOER</h1>
       <div className={styles.text}>
-      <p>PCCOER ACM Student Chapter has been founded for the technical development of students in the field of computing.
-          The Chapter provides a platform for students to showcase their talents.It facilitates community and individual growth in the computing profession.
-           The Chapter organizes a number of events,seminars,talks, and workshops for its members.It aims to promote Computer Science Culture.</p>
+      <p>PCCOER ACM Student Chapter has been founded for the technical development of students in the field of computing.The Chapter provides a platform for students to showcase their talents.It facilitates community and individual growth in the computing profession.The Chapter organizes a number of events,seminars,talks, and workshops for its members.It aims to promote Computer Science Culture.</p>
        </div>
     </div>
    );
