3f110c3d7a03e22feb14cc3fecb5e50288979d0d
yashdkadam <kadamdyash55@gmail.com>
2023-02-17 13:13:31 +0530

    initial commit

--- pages/events.js
+++ pages/events.js
@@ -1,3 +1,36 @@
+import Slider from "react-slick";
+import styles from "../styles/Events.module.css";
+
 export default function Events() {
-  return <h1>Events page</h1>;
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    fade: true,
+    dotsClass: "slick-dots " + styles.dots,
+  };
+  return (
+    <div className={styles.sliderContainer}>
+      <h2> Gallery</h2>
+      <Slider {...settings}>
+        <div>
+          <img src="/acm_1.png" />
+        </div>
+        <div>
+          <img src="/acm_2.png" />
+        </div>
+        <div>
+          <img src="/acm_3.png" />
+        </div>
+        <div>
+          <img src="/acm_4.png" />
+        </div>
+      </Slider>
+    </div>
+  );
 }
+
+
