dec6abe432d22bb940474b73705fa12007b1e01d
dazkool69 <nvkulkarni.1108@gmail.com>
2023-03-16 22:45:21 +0530

    responsive navbar complete

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,37 @@
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className={styles.navbar}>
-    <div className={styles.logo}>
-     <Image src="/acmLogo1.png" width={200} height={65} />
-    </div>
-      <Link href="/" className={styles.content}>
-        Home
-      </Link>
-      <Link href="/about" className={styles.content}>
-        About
-      </Link>
-      <Link href="/team" className={styles.content}>
-        Team
-      </Link>
-      <Link href="/blog" className={styles.content}>
-        Blog
-      </Link>
-      <Link href="/events" className={styles.content}>
-        Events
-      </Link>
-    </nav>
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
+        <div>
+          <Image
+            src="/acmLogo.png"
+            width={200}
+            height={60}
+            alt="main-logo"
+            className={styles.logo}
+            priority={true}
+          />
+        </div>
+
+        <div className={styles.menu}>
+          <Link href="/about" className={styles.content}>
+            About
+          </Link>
+          <Link href="/team" className={styles.content}>
+            Team
+          </Link>
+          <Link href="/blog" className={styles.content}>
+            Blog
+          </Link>
+          <Link href="/events" className={styles.content}>
+            Events
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 }
