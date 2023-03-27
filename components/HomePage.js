7964930a77d9238fef893122b0dfc3b62c73f2d7
pranjal.more_comp21 <pranjal.more_comp21@pccoer.in>
2023-03-27 22:26:51 +0530

    home page updated

--- components/HomePage.js
+++ components/HomePage.js
@@ -2,10 +2,22 @@ import styles from "../styles/HomePage.module.css";
 
 export default function HomePage() {
   return (
+    <>
+    
     <div className={styles.container}>
-      <h1 className={styles.title}>BE INNOVATIVE</h1>
-      <h1 className={styles.title}>BE DIFFERENT</h1>
-      <h2 className={styles.subtitle}>Advancing Through Technology</h2>
+          <img className={styles.homepageChild} alt="" src="/svg/ellipse-32.svg" />
+          <img className={styles.ellipseIcon} alt="" src="/svg/ellipse-52.svg" />
+          <img className={styles.homepageChild1} alt="" src="/svg/ellipse-42.svg" />
+       <div  className={styles.container1}>
+          <h1 className={styles.title}>BE INNOVATIVE</h1>
+          <h1 className={styles.title}>BE DIFFERENT</h1>
+          <h2 className={styles.subtitle}>Advancing Through Technology</h2>
+       </div>
+        <img className={styles.homepageItem} alt="" src="/svg/ellipse-22.svg" />
+        <img className={styles.homepageInner} alt="" src="/svg/ellipse-12.svg" />
     </div>
+    
+    </>
+    
   );
 }
