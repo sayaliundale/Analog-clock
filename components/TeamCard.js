70849f8b3a40fcaaf24b10162025c527a738f2f6
Trapti <trapti.gorbade_comp21@pccoer.in>
2023-03-27 23:05:30 +0530

    Team data updated

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -1,6 +1,7 @@
 import styles from "../styles/Team.module.css";
 import Image from "next/image";
 
+
 const TeamCard = (props) => {
   return (
     <div className={styles.card}>
@@ -9,12 +10,30 @@ const TeamCard = (props) => {
         width={200}
         height={60}
         alt="main-logo"
-        className={styles.logo}
+        className={styles.img}
         priority={true}
+        
       />
-      <div className={styles.textbox}>
-        <p className={`${styles.text} ${styles.head}`}>{props.name}</p>
+      <div className={styles.textBox}>
+        <p className={`${styles.text} ${styles.head} `}>{props.name}</p>
         <span>{props.title}</span>
+        {props.icons && props.icons.length > 0 && (
+          <span className={styles.icons}>
+           {props.icons.map((icon, index) => (
+              <span key={index} className={styles.icon}>
+                <Image
+                  src={icon.src}
+                  alt={icon.alt}
+                  width={icon.width}
+                  height={icon.height}
+                />
+              </span>
+            ))}
+          </span>
+        )}
+      
+ 
+
       </div>
     </div>
   );
