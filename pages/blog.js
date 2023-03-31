b167d3d183287e7cc88c104a05f5e8412a6ad25d
pranjal.more_comp21 <pranjal.more_comp21@pccoer.in>
2023-03-31 17:21:30 +0530

    changes

--- pages/blog.js
+++ pages/blog.js
@@ -1,3 +1,12 @@
+import styles from '../styles/Blog.module.scss'
+
+
 export default function Blog() {
-  return <h1>Blog page</h1>;
+  return(
+    <>
+    <div className={styles.blurBg}></div> 
+     <h1>Blog page</h1>
+    <div className={styles.blurBg2}></div> 
+    </>
+     );
 }
