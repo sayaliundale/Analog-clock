b81b9c8d11cccf5304aadef8d6a758f73fe5039c
Trapti <trapti.gorbade_comp21@pccoer.in>
2023-03-31 16:47:15 +0530

    team css

--- components/TeamCard.js
+++ components/TeamCard.js
@@ -1,7 +1,6 @@
 import styles from "../styles/Team.module.css";
 import Image from "next/image";
 
-
 const TeamCard = (props) => {
   return (
     <div className={styles.card}>
@@ -12,31 +11,28 @@ const TeamCard = (props) => {
         alt="main-logo"
         className={styles.img}
         priority={true}
-        
       />
       <div className={styles.textBox}>
         <p className={`${styles.text} ${styles.head} `}>{props.name}</p>
         <span>{props.title}</span>
         {props.icons && props.icons.length > 0 && (
           <span className={styles.icons}>
-           {props.icons.map((icon, index) => (
-              <span key={index} className={styles.icon}>
+            {props.icons.map((icon, index) => (
+              <a href={icon.href} key={index} className={styles.icon}>
                 <Image
                   src={icon.src}
                   alt={icon.alt}
                   width={icon.width}
                   height={icon.height}
                 />
-              </span>
+              </a>
             ))}
           </span>
         )}
-      
- 
-
       </div>
     </div>
   );
 };
 
 export default TeamCard;
+
