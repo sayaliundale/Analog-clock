ade94f75b41a8ec6c867ded1400b4cc0961b88a1
dazkool69 <nvkulkarni.1108@gmail.com>
2023-05-04 11:45:10 +0530

    fixed teams page image loading problem

--- pages/team.js
+++ pages/team.js
@@ -5,22 +5,22 @@ import styles from "../styles/Team.module.css";
 export default function Team() {
   const admin = Admindata.map((admin) => {
     return (
-      <div className={styles.container1}>
-        <TeamCard key={admin.id} {...admin} />
+      <div key={admin.id} className={styles.container1}>
+        <TeamCard {...admin} />
       </div>
     );
   });
   const members = teamsData.map((member) => {
     return (
-      <>
-
-      <div className={styles.container}>
-        <TeamCard key={member.id} {...member} />
+      <div key={member.id} className={styles.container}>
+        <TeamCard {...member} />
       </div>
-
-
-      </>
     );
   });
-  return <div><div className={styles.container}>{admin}</div><div className={styles.container}>{members}</div></div>;
+  return (
+    <div>
+      <div className={styles.container}>{admin}</div>
+      <div className={styles.container}>{members}</div>
+    </div>
+  );
 }
